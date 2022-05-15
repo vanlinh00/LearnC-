b0f5b0a54b375eb6b1b76256024cdbcb8c147f6d
vanlinh00 <vanlinh20192019@gmail.com>
2022-05-16 00:16:28 +0700

    Substring Check- convert arraylist to array int

--- basic/Program.cs
+++ basic/Program.cs
@@ -26,7 +26,7 @@ namespace MyApplication
             {
                 Console.WriteLine(arrayInt[i]);
             }
-            
+
 
             int[] arrayInt = new int[4];
             for(int i=0;i<arrayInt.Length;i++)
@@ -37,7 +37,7 @@ namespace MyApplication
             {
                 Console.WriteLine(arrayInt[i]);
             }
-         */
+            */
             /*
              * OOP 
                 car car1 = new car();
@@ -81,90 +81,169 @@ namespace MyApplication
             }
             */
 
-             
-             
-            int[] line=new int[1001];
-            int[] arrayPTtrung = new int[1001];
-          
-            int n = Convert.ToInt32(Console.ReadLine());
-            int[] phanTudaLoc = new int[n];
 
-            var arlist1 = new ArrayList();
-            for (int i = 0; i < n; i++)
-            {
-                line[i] = Convert.ToInt32(Console.ReadLine());
-            }
-            int dem = 0;
-            for (int i = 0; i < n; i++)
-            {
-                for (int j = i + 1; j < n; j++)
+            /* Bài toán tìm hình chữ nhật , có cạnh là lớn nhất     
+
+                int[] line=new int[1001];
+                int[] arrayPTtrung = new int[1001];
+
+                int n = Convert.ToInt32(Console.ReadLine());
+                int[] phanTudaLoc = new int[n];
+
+
+                for (int i = 0; i < n; i++)
                 {
-                    if (line[i] == line[j])
+                    line[i] = Convert.ToInt32(Console.ReadLine());
+                }
+
+                for (int i = 0; i < n; i++)
+                {
+                    for (int j = i + 1; j < n; j++)
                     {
-                        dem++;
-                        arrayPTtrung[line[i]] += 1;
+                        if (line[i] == line[j])
+                        {
+
+                            arrayPTtrung[line[i]] += 1;
+                        }
+
                     }
 
                 }
-                if(dem==0)
-                {
-                    arlist1.Add(line[i])  ;
-                }
-                dem = 0;
-            }
-            /*
-             arraylist
+                  int max1 = 0;
+                int max2 = 0;
 
+                // vì nó chạy từ bé đến lớn mời tìm max min kiểu này
+                for(int i=0;i<arrayPTtrung.Length;i++)
+                {
+                   if(arrayPTtrung[i] >=1 && i>max1 )
+                    {
+                        max2 = max1;
+                        max1 = i;
+                     }
+                }
+
+                if(max1*max2==0)
+                {
+                    Console.WriteLine("-1");
+                }
+                else
+                {
+                 //   int s=((max1!=0 && max2==0):)
+                    Console.WriteLine(max1 * max2);
+                }
+
+                search2Max(line);
+            */
+
+            /*
+            arraylist
+            ar arlist1 = new ArrayList();
             Console.WriteLine("\n");
             Console.WriteLine("\n");
             arlist1.Sort();
             foreach (var i in arlist1)
             {
-             //   Console.WriteLine(i);
+            //   Console.WriteLine(i);
             }
             // Console.WriteLine("\n");
+
+
+            ArrayList arrlist1 = new ArrayList();
+            var arrlist2 = new ArrayList();
+            arrlist1.Add(1);
+            arrlist1.Add(2);
+            arrlist2.Add(3);
+            arrlist1.Add("hello world");
             */
 
-            int max1 = 0;
-            int max2 = 0;
+            //------------------------------------------------------
+            /* 
+             tìm ra câu trả lời cho sự sống, vũ trụ và vạn vật.
+            Chính xác hơn là ... viết lại các số nhỏ từ đầu vào đến đầu ra. 
+            Dừng xử lý đầu vào sau khi đọc số 42.
+            Tất cả các số ở đầu vào là số nguyên có một hoặc hai chữ số.
+            
 
-            // vì nó chạy từ bé đến lớn mời tìm max min kiểu này
-            for(int i=0;i<arrayPTtrung.Length;i++)
+            int[] a = { 1, 2, 4, 6, 42,99 };
+            for(int i =0;i<a.Length;i++)
             {
-               if(arrayPTtrung[i] >=1 && i>max1 )
+                if(a[i]==42)
                 {
-                    max2 = max1;
-                    max1 = i;
-                 }
-            }
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine(a[i]);
+                }
+            }    
+            */
 
-            if(max1*max2==0)
+            //------------------------------------------------
+            /* check có phải là chuỗi con khong
+             * SBSTR1 - Substring Check (Bug Funny)
+                 1010110010 10110
+                 1110111011 10011
+                 First two lines of output:
+                    1
+                    0
+             
+
+            int[] A = { 1, 0, 1, 0, 1, 1, 0, 0, 1, 0 };
+            int[] B = { 1, 0, 1, 1, 0 };
+            //  int[] A = { 1, 1, 1, 0, 1, 1, 1, 0, 1, 1 };
+            //   int[] B = { 1, 0, 0, 1, 1 };
+
+            //   4 - 2 + 1;
+            //    4-1+1
+            //   4-3+1
+            //   10-5+1
+            ArrayList arrayList = new ArrayList();
+            int vonglap = 0;
+            int dem = 0;
+            int ichuan = 0;
+            int checkMangCon = 0;
+            while (vonglap < A.Length - B.Length + 1)
             {
-                Console.WriteLine("-1");
+                for (int i = ichuan; i < B.Length + dem; i++)
+                {
+                    arrayList.Add(A[i]);
+                }
+                int[] myArr = (int[])arrayList.ToArray(typeof(int));
+
+                if (myArr.Length == B.Length && soSanh2Array(B, myArr))
+                {
+                    checkMangCon++;
+                    break;
+                }
+                dem++;
+                ichuan++;
+                vonglap++;
+                arrayList.Clear();
+            }
+            if (checkMangCon == 1)
+            {
+                Console.WriteLine("1");
             }
             else
             {
-             //   int s=((max1!=0 && max2==0):)
-                Console.WriteLine(max1 * max2);
+                Console.WriteLine("0");
             }
-
-            search2Max(line);
-       }
-       static void search2Max(int[] array)
+            */
+        }
+        static void search2Max(int[] array)
         {
             int max1 = 0;
             int max2 = 0;
-            for(int i=0;i<array.Length;i++)
+            for (int i = 0; i < array.Length; i++)
             {
-                if(array[i]>max1)
+                if (array[i] > max1)
                 {
-                   
                     max1 = array[i];
-                }    
+                }
             }
             for (int i = 0; i < array.Length; i++)
             {
-                if (array[i] > max2&& array[i]<max1)
+                if (array[i] > max2 && array[i] < max1)
                 {
 
                     max2 = array[i];
@@ -175,11 +254,23 @@ namespace MyApplication
             Console.WriteLine(max2);
             Console.WriteLine(max1);
         }
-   }
+        static bool soSanh2Array(int[] array1, int[] array2)
+        {
+            for (int i = 0; i < array1.Length; i++)
+            {
+                if (array1[i] != array2[i])
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+    }
     public class car
     {
-           public float speed;
-           public string color;
+        public float speed;
+        public string color;
     }
 
 }
